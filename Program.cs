1b33dc2264b3ebf290fb8662da12f9bfefa458cb
Семён <sema.panov1@gmail.com>
2024-05-28 11:56:00 +0300

    Добавил функцию для вывода исходного массива в консоль

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿string[] array = { "Hello", "2", "world", ":-)" };
-int maxLength = 3;
+int maxSize = 3;
+PrintArray(array);
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
